5d65be7d60616d7ababff80397b60d21f1c67266
wildjames <jwild2@sheffield.ac.uk>
2020-02-17 17:04:02 +0000

    Observed with ucam on the ntt

--- calc_spec_mag.py
+++ calc_spec_mag.py
@@ -7,11 +7,11 @@ from ucam_thruput import _make_instrument_reference_table, getref
 
 # This is the bandpass in the relevant lightpath
 telescope, instrument = "ntt", "ucam"
-filters = ["u_s", 'g_s', 'r_s']
+filters = ["u", 'g', 'r', 'i', 'z']
 
 # The number in Pickles - http://www.stsci.edu/hst/instrumentation/reference-data-for-calibration-and-tools/astronomical-catalogs/pickles-atlas
 
-# HD 121968 -
+# HD 121968 - B2ii star http://simbad.u-strasbg.fr/simbad/sim-id?Ident=HD+121968&submit=submit+id
 N = 106
 plax = 0.2921
 
@@ -30,6 +30,7 @@ sp = orig_sp.addmag(dmod)
 
 # TODO: Do I need to integrate over the star's profile?
 
+mags = {}
 for filt in filters:
     # Get the lightpath
     bp = S.ObsBandpass('{},{},{}'.format(telescope, instrument, filt))
@@ -38,10 +39,21 @@ for filt in filters:
 
     # Convert to magnitude
     ADU_flux = obs.countrate()
-    mag = -2.5 * np.log10(ADU_flux)
+    mag = obs.effstim('abmag')
 
+    mags[filt] = mag
 
     print("Band: {}".format(filt))
     print("Countrate: {:.3f}".format(ADU_flux))
-    print("Instrumental mag: {:.3f}\n".format(mag))
+    print("abmag: {:.3f}\n".format(mag))
 
+ug = mags['u'] - mags['g']
+gr = mags['g'] - mags['r']
+ri = mags['r'] - mags['i']
+iz = mags['i'] - mags['z']
+
+print("r: {:.3f}".format(mags['r']))
+print("u - g: {:.3f}".format(ug))
+print("g - r: {:.3f}".format(gr))
+print("r - i: {:.3f}".format(ri))
+print("i - z: {:.3f}".format(iz))
